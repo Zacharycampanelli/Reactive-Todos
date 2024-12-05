a1b67ba18555f0e1eebdb8abe826842689a8c8b8
Zachary Campanelli <zackjcampanelli@gmail.com>
2024-12-04 23:15:03 -0500

    Initial color setup with ability to change themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { useState } from "react"
 
-
+import './index.css'
 function App() {
-  
+  const [theme, setTheme] = useState('light')
 
   return (
-    <>
+    <div className={`${theme} p-6`}>
+      <div className="bg-background w-full">
+    <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>change</button>
+      </div>
      
-    </>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "rgb(var(--background))",
+        taskBox: "rgb(var(--task-box))",
+        activeTask: "rgb(var(--active-task))",
+        finishedTask: "rgb(var(--finished-task))",
+        primaryText: "rgb(var(--primary-text))",
+        dividerCircle: "rgb(var(--divider-circle))",
+        hover: "rgb(var(--hover))",
+
+        currentPage: "rgb(var(--current-page))",
+        gradientStart: "rgb(var(--gradient-start))",
+        gradientEnd: "rgb(var(--gradient-end))",
+      }
+    },
   },
   plugins: [],
 }
 
+  
